56642ce48ace8f9c993833a5feaaab64de5f38dd
krishnakumarkp <krishnakumarkp@gmail.com>
2020-09-04 13:17:47 +0000

    Generated at Fri Sep  4 13:17:36 UTC 2020 with swagger file version 20200904.045000

--- generator.sh
+++ generator.sh
@@ -3,15 +3,51 @@
 SCRIPT="$0"
 echo "# START SCRIPT: $SCRIPT"
 
+COMMIT="true"
+SWAGGER_VERSION=
+SDK_VERSION=
 
-PARAMS_VERSION="0.1"
+echo_usage() {
+	echo "Usage: $0 -s swagger_version -c commit"
+	echo ""
+	echo "-s swagger_version the tag of the swagger file in it git repo"
+	echo "-c commit the generated code in to github repo [OPTIONAL], default: true"
+	echo "-h  print help"
+}
+
+while getopts :s:c:h opt; do
+  case $opt in
+    s) 
+		SWAGGER_VERSION=$OPTARG 
+		;;
+    c) 
+		COMMIT=$OPTARG 
+		;;
+    h) 
+		echo_usage 
+		exit 0 
+		;;
+	\?)
+		echo "Unknown option: -$OPTARG"
+		echo_usage
+		exit 1
+		;;
+  esac
+done
+
+CURRENTDATETIME=`date +"%Y%m%d.%H%M%S"`
+SDK_VERSION="${SWAGGER_VERSION}:${CURRENTDATETIME}"
 
 URL="swagger_file/openapi.json"
 
 SDK_GEN_FOLDER="sdk-php"
 SDK_REPO_FOLDER="sdk-repo"
+SWAGGER_REPO_FOLDER="swagger_file"
+SWAGGER_REPO="https://github.com/krishnakumarkp/bookstore-swagger.git"
+SDK_REPO="https://github.com/krishnakumarkp/sdk-php.git"
 
-echo " version: $PARAMS_VERSION " 
+echo "generating sdk version $SDK_VERSION"
+COMMIT_MSG="Generated at $(date) with swagger file version $SWAGGER_VERSION"
 
 delete_folder_if_exists(){
     folder=$1
@@ -25,16 +61,32 @@ delete_folder_if_exists(){
 #delete folder before the build
 delete_folder_if_exists $SDK_GEN_FOLDER
 
+#this is to checkout the swagger file
+delete_folder_if_exists $SWAGGER_REPO_FOLDER
+
+git clone --depth 1 --branch $SWAGGER_VERSION $SWAGGER_REPO $SWAGGER_REPO_FOLDER
+echo "clone $SWAGGER_VERSION version of swaggerfile to $SWAGGER_REPO_FOLDER"
+
+if [ ! -d "$SWAGGER_REPO_FOLDER" ]; then
+  echo "Error: ${SWAGGER_REPO_FOLDER} not found. Can not continue."
+  exit 1
+fi
+
+echo "checked out swagger file"
+
 executable="./generator_cli/openapi-generator-cli.jar"
 
-java -jar $executable generate -t ./templates/swagger/php -i $URL -g php -o $SDK_GEN_FOLDER --package-name "Book store APIs Client Library for PHP" --invoker-package Bookstore\\Client --artifact-version $PARAMS_VERSION --git-user-id Krishnakumarkp --git-repo-id sdk-php  --skip-validate-spec
+java -jar $executable generate -t ./templates/swagger/php -i $URL -g php -o $SDK_GEN_FOLDER --package-name "Book store APIs Client Library for PHP" --invoker-package Bookstore\\Client --artifact-version $SDK_VERSION --git-user-id Krishnakumarkp --git-repo-id sdk-php --skip-validate-spec
 
 if [ ! -d "$SDK_GEN_FOLDER" ]; then
   echo "Error: ${SDK_GEN_FOLDER} not found. Can not continue."
   exit 1
 fi
 
-#mkdir $SDK_REPO_FOLDER
+#checkout the repo for generated code
+
+git clone $SDK_REPO $SDK_REPO_FOLDER
+echo "clone $SDK_REPO_FOLDER"
 
 if [ ! -d "$SDK_REPO_FOLDER" ]; then
   echo "Error: ${SDK_REPO_FOLDER} not found. Can not continue."
@@ -48,3 +100,14 @@ find -maxdepth 1 ! -name .git ! -name . -exec rm -rv {} \;
 #copy the generated code into repo
 cp -a ../$SDK_GEN_FOLDER/. .
 echo "copy $SDK_GEN_FOLDER to $SDK_REPO_FOLDER "
+#start commit
+
+if [ "$COMMIT" = "true" ]; then
+	echo "$COMMIT_MSG"
+	git add .
+	git commit -a -m "$COMMIT_MSG"
+	git push origin master
+	git tag $SDK_VERSION
+	git push origin $SDK_VERSION
+	echo "git pushed"
+fi
